69bab03696bbf8ac4f7f1bb6979aef376369e65e
marjorry <marjorie.s@escolar.ifrn.edu.br>
2024-08-26 11:53:07 -0300

    fase 1

--- Meu Game 3D/Assets/scripts came/script/Player.cs
+++ Meu Game 3D/Assets/scripts came/script/Player.cs
@@ -2,12 +2,17 @@ using Vector3 = UnityEngine.Vector3;
 using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
+using JetBrains.Annotations;
 using Unity.VisualScripting;
-using UnityEngine;
+using UnityEngine; 
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     public int velocidade = 10;
+    public int forcaPulo = 7;
+    public bool noChao;
+    
     public Rigidbody rb;
     // Start is called before the first frame update
     void Start()
@@ -25,6 +30,21 @@ public class Player : MonoBehaviour
 
         Vector3 direcao = new Vector3(x,0,y);
         rb.AddForce(direcao * velocidade * Time.deltaTime, ForceMode.Impulse);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * forcaPulo, ForceMode.Impulse);
+        }
+        
+        
+        
+
+
+        if (transform.position.y <= -10)
+        {
+            //jogador caiu
+           SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
     }
 }
 
