610fab8f4bf1394c9ba6ff689189c8d0aed60a0f
marjorry <marjorie.s@escolar.ifrn.edu.br>
2024-09-03 10:21:15 -0300

    moeda 5/7

---
+++ Meu Game 3D/Assets/scripts came/script/Moeda.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
+public class Moeda : MonoBehaviour
+ 
+{
+public int velocidadeGiro = 10;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+void Update()
+    { 
+        transform.Rotate(Vector3.forward * velocidadeGiro * Time.deltaTime);
+    }
+}
